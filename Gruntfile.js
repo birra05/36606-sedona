9964a331aeeb85ca27b59cf2d4a78b1c31df0498
birra05 <birra05@mail.ru>
2015-10-26 21:02:36 +0300

    Сборка проекта

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
 
   var config = {
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     less: {
       style: {
         files: {
-          'css/style.css': 'less/style.less'
+          "build/css/style.css": "source/less/style.less"
         }
       }
     },
@@ -17,28 +17,75 @@ module.exports = function(grunt) {
     postcss: {
       options: {
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'})
+          require("autoprefixer")({browsers: "last 2 versions"})
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: "build/css/*.css"
       }
     },
 
     watch: {
       style: {
-        files: ['less/**/*.less'],
-        tasks: ['less', 'postcss'],
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss", "cssmin"],
         options: {
           spawn: false,
           livereload: true
         }
       }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: "gzip"
+      },
+      style: {
+        files: {
+          "build/css/style.min.css": "build/css/style.css"
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: "source/img/**/*.{png,jpg,gif,svg}"
+        }]
+      }
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: ["img/**","js/**","*.html"],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
     }
   };
 
-  config = require('./.gosha')(grunt, config);
+  config = require("./.gosha")(grunt, config);
 
   grunt.initConfig(config);
 
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "cssmin",
+    "imagemin"
+  ]);
 };
